446f3cec9c68764e9999f7745b6efcbbbf1605ce
Carlos del Hoyo Muñoz <cdelhoyo@autentia.com>
2019-02-19 17:12:15 +0100

    Con el cursor en el for -> Replace with collect

--- src/main/java/pipelines/Author.java
+++ src/main/java/pipelines/Author.java
@@ -8,20 +8,19 @@ https://martinfowler.com/articles/refactoring-pipelines.html
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Author {
     public String company;
     public String twitterHandle;
 
     static public List<String> twitterHandles(List<Author> authors, String company) {
-        List<String> result = new ArrayList();
-        for (Author a: authors) {
-            if (a.company == company) {
-                String handle = a.twitterHandle;
-                if (handle != null)
-                    result.add(handle);
-            }
-        }
+        List<String> result = authors.stream()
+                .filter(author -> author.company == company)
+                .map(author -> author.twitterHandle)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
         return result;
     }
 }
