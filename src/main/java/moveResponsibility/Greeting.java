e3ca8d651ccd43be92d3bb7a08a005a005f45893
Carlos del Hoyo Muñoz <cdelhoyo@autentia.com>
2019-02-19 13:10:00 +0100

    delegate

---
+++ src/main/java/moveResponsibility/Greeting.java
@@ -0,0 +1,12 @@
+package moveResponsibility;
+
+public class Greeting {
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void sayBye() {
+        System.out.println("Good bye!");
+    }
+}
